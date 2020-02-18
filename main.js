7aa5c3203848dd6104a01ef6cb2ae1109d743c13
DylanHR <hrdylan@gmail.com>
2020-02-18 11:30:24 -0500

    adds js reveal animation

---
+++ main.js
@@ -0,0 +1,17 @@
+var slideUp2 = {
+    distance: '20%',
+    origin: 'bottom',
+    opacity: 0,
+    duration: 1000
+};
+
+var slideUp1 = {
+    distance: '20%',
+    origin: 'bottom',
+    opacity: 0,
+    duration: 2000
+};
+
+ScrollReveal().reveal('#right', slideUp1)
+ScrollReveal().reveal('#left', slideUp2)
+ScrollReveal().reveal('#main-panel', slideUp2)
